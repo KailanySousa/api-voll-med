8245ed1a49c486d7e0f3a5ddb7fef9545909b7ce
KailanySousa <kailany-sousa2010@hotmail.com>
2024-06-16 14:14:01 -0300

    feat: mapeando entidade Endereco

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,19 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+}
