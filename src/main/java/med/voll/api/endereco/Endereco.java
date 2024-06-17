38b123414d5690cecf7d48d4e397d062c429baf7
KailanySousa <kailany-sousa2010@hotmail.com>
2024-06-17 16:22:45 -0300

    feat: implementando o atualizar medico

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,7 +1,10 @@
 package med.voll.api.endereco;
 
 import jakarta.persistence.Embeddable;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Embeddable
 @Getter
@@ -25,5 +28,15 @@ public class Endereco {
         this.cidade = dados.cidade();
         this.complemento = dados.complemento();
         this.numero = dados.numero();
+    }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if (dados.logradouro() != null) this.logradouro = dados.logradouro();
+        if (dados.bairro() != null) this.bairro = dados.bairro();
+        if (dados.cep() != null) this.cep = dados.cep();
+        if (dados.uf() != null) this.uf = dados.uf();
+        if (dados.cidade() != null) this.cidade = dados.cidade();
+        if (dados.numero() != null) this.numero = dados.numero();
+        if (dados.complemento() != null) this.complemento = dados.complemento();
     }
 }
