980dc7ab75db993d4d5fcb2d93e4f2d1bb5d60d1
KailanySousa <kailany-sousa2010@hotmail.com>
2024-06-19 14:40:01 -0300

    feat: implementando o get para buscar os detalhes do medico

--- src/main/java/med/voll/api/dtos/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/dtos/DadosDetalhamentoMedico.java
@@ -5,8 +5,8 @@ import med.voll.api.entities.Especialidade;
 import med.voll.api.entities.Medico;
 
 public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone,
-                                      Especialidade especialidade, Endereco endereco) {
+                                      Especialidade especialidade, Endereco endereco, Boolean ativo) {
     public DadosDetalhamentoMedico(Medico medico) {
-        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco(), medico.getAtivo());
     }
 }
